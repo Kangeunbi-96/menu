e530502a277a4bedea4bb81fe157418dc3d53ac8
Kang-eun-bi <54533996+Kang-eun-bi@users.noreply.github.com>
2020-01-08 10:31:04 +0900

    menu

--- ex2/index11.js
+++ ex2/index11.js
@@ -17,67 +17,68 @@ window.addEventListener("load", function(){
             JSON.parse(this.responseText);
         }
         a = JSON.parse(this.responseText);
-
-            var template1 = section1.querySelector("#template1");
-            var i = 0;
-            for(i=0; i<a.categoryResponses.length; i++) {
+        
+        function insert() {
+            for(var i=0; i<a.categoryResponses.length; i++) {
                 var cloneNode = document.importNode(template1.content, true);
                 var tds = cloneNode.querySelectorAll("td");
-                
                 tds[0].textContent = a.categoryResponses[i].categoryNo;
                 tds[1].textContent = a.categoryResponses[i].name; 
                 tbodyNode.appendChild(cloneNode);
+            }
+        }
+        insert();
+
+        function f_test(category, categoryNo, depth) {
+            category.find(data => {
+                if(data.categoryNo === categoryNo) {
+                    if(depth === 's1') {
+                        child(template2, category, categoryNo, data, tbodyNode2);
+                    } else if(depth === 's2') {
+                        child(template3, category, categoryNo, data, tbodyNode3);
+                    }
+                    return category;
+                } else {
+                     return f_test(data.children, categoryNo, depth);
+                }
+            })
         }
 
-        function test(category, categoryNo) {
-            tbodyNode2.innerHTML = "";
-            tbodyNode3.innerHTML = "";
-             category.find(data => {
-                if(data.categoryNo === categoryNo) {
-                    var cloneNode = document.importNode(template2.content, true);
-                    var tds = cloneNode.querySelectorAll("td");
-                    for(var i = 0; i < category.length; i++) {
-                        if(data.categoryNo === category[i].categoryNo) {
-                            tds[0].textContent = category[i].children[0].categoryNo;
-                            tds[1].textContent = category[i].children[0].name;
-                            tbodyNode2.appendChild(cloneNode);}} return category;}
-                            else {
-                                return test(data.children, categoryNo);
-                            }
-                        })}
+        function child(num, category, categoryNo, data, num2) {
+            var cloneNode = document.importNode(num.content, true);
+            var tds = cloneNode.querySelectorAll("td");
 
-        function test2(category, categoryNo) {
-            category.find(data => {
-               if(data.categoryNo === categoryNo) {
-                   var cloneNode = document.importNode(template3.content, true);
-                   var tds = cloneNode.querySelectorAll("td");
+            for(var i = 0; i < category.length; i++) {
+                if(data.categoryNo === category[i].categoryNo) {
+                    tds[0].textContent = category[i].children[0].categoryNo;
+                    tds[1].textContent = category[i].children[0].name;
+                    num2.appendChild(cloneNode);
+                 }
+             }
+        }
 
-                   for(var i = 0; i < category.length; i++) {
-                       if(data.categoryNo === category[i].categoryNo) {
-                           tds[0].textContent = category[i].children[0].categoryNo;
-                           tds[1].textContent = category[i].children[0].name;
-                           tbodyNode3.appendChild(cloneNode);}} return category;}
-                           else {
-                               return test2(data.children, categoryNo);
-                            }
-                        })}
-
-        for(var i=0; i<a.categoryResponses.length; i++) {
-            if(tbodyNode.children[i].onclick = function() {
-                // this.onclick = null;
-                // this.style.backgroundColor = "yellow";
+        function click1(num) {
+            num.onclick = function(e) {
+                tbodyNode2.innerHTML = "";
+                tbodyNode3.innerHTML = "";
                 var toto = Number(this.children[0].textContent);
-                test(a.categoryResponses, toto);
-                for(var i=0; i<tbodyNode2.children.length; i++) {
-                    if(tbodyNode2.children[i].onclick = function() {
+                f_test(a.categoryResponses, toto, this.parentNode.parentNode.parentNode.className);
+                function click2(num) {
+                    num.onclick = function(e) {
                         this.onclick = null;
-                        // this.style.backgroundColor = "yellow";
-                        var toto2 = Number(this.children[0].textContent);
-                        test2(a.categoryResponses, toto2);
-                    });
+                        var toto = Number(this.children[0].textContent);
+                        f_test(a.categoryResponses, toto, this.parentNode.parentNode.parentNode.className);
+                    }
                 }
-            }); 
+                for(var i = 0; i < tbodyNode2.children.length; i++) {
+                    click2(tbodyNode2.children[i]);
+                }
+            }
+        }
+        for(var i = 0; i < a.categoryResponses.length; i++) {
+            click1(tbodyNode.children[i]);
         }
     }
+
     xhr.send();
 });
